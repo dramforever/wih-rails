70969ffb25c796a7d2d7fc2669b2267a7d8f5677
Leonard Hu <hypernovagama@gmail.com>
2013-06-23 16:09:45 +0800

    初始化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WihRails::Application.config.secret_token = '0d56bb65353dc97729761ee4038ad11461925baaad7ed1ccf1c7ec150f2fcb20190cba590b1ba07293013a88c05253d1827eeb0a57dff95b4939955f514bd2a3'
