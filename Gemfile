1c8d8beff56629c87c21b50b99bab5405c72e54b
Leonard Hu <hypernovagama@gmail.com>
2013-06-23 16:21:27 +0800

    去除test模块

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'http://ruby.taobao.org'
+source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 
@@ -18,6 +18,11 @@ group :assets do
   # gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'cucumber-rails', :require => false
+  gem 'database_cleaner'
 end
 
 gem 'jquery-rails'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WihRails::Application.config.secret_token = '0d56bb65353dc97729761ee4038ad11461925baaad7ed1ccf1c7ec150f2fcb20190cba590b1ba07293013a88c05253d1827eeb0a57dff95b4939955f514bd2a3'
+WihRails::Application.config.secret_token = '2ea76d94e16f70958ab9ab900991b07f5e11e9cf624114d9e14ae769342d20b281c435d99bf9c43af3308f9607d19c5019801991141fe3e0a22983fdcd6fa49a'
